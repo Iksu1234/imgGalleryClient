96c23ac2d190b0acf62c3cd049516866f31cdda4
Teemu <teemuikonen@live.fi>
2025-03-17 14:52:55 +0200

    fixed header spacings

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
-import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -6,20 +7,43 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 function Header() {
   return (
     <>
-      <Navbar expand="lg" className="jumbotron bg-primary full-width-header">
-        <Container>
-          <Navbar.Brand>Image Gallery</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="Admin controls" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/add_images">
-                  Add images
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+      <Navbar
+        expand="lg"
+        className="jumbotron bg-primary full-width-header header-font"
+        style={{ height: "10vh" }}
+      >
+        <Row className="w-100">
+          <Col>
+            <div className="float-start mx-3">
+              <img
+                alt=""
+                src="https://react-bootstrap.netlify.app/img/logo.svg"
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+              />
+            </div>
+          </Col>
+          <Col>
+            <Navbar.Brand className="m-0">
+              <h2>Image Gallery</h2>
+            </Navbar.Brand>
+          </Col>
+          <Col>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <div className="float-end mx-3">
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <NavDropdown title="Admin controls" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/add_images">
+                      Add images
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          </Col>
+        </Row>
       </Navbar>
     </>
   );
